36e3fd580a3de0c8496d448d3eab50bad5274fd4
JatinBumbra <56374584+JatinBumbra@users.noreply.github.com>
2020-08-31 13:38:55 +0530

    Admin CRUD users

---
+++ controllers/users.js
@@ -0,0 +1,53 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/asyncHandler');
+const User = require('../models/User');
+
+// @route     GET /api/v1/users
+// @desc      Get all users
+// @access    Private/Admin
+exports.getUsers = asyncHandler(async (req, res, next) => {
+	res.status(200).json(res.advancedResults);
+});
+
+// @route     GET /api/v1/users/:id
+// @desc      Get all users
+// @access    Private/Admin
+exports.getUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.params.id);
+	if (!user) {
+		return next(
+			new ErrorResponse(`User with id ${req.params.id} not found`, 404)
+		);
+	}
+	res.status(200).json({ success: true, data: user });
+});
+
+// @route     POST /api/v1/users
+// @desc      Create user
+// @access    Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+	const user = await User.create(req.body);
+
+	res.status(201).json({ success: true, data: user });
+});
+
+// @route     PUT /api/v1/users/:id
+// @desc      Update user
+// @access    Private/Admin
+exports.updateUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({ success: true, data: user });
+});
+
+// @route     DELETE /api/v1/users/:id
+// @desc      Delete user
+// @access    Private/Admin
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+	await User.findByIdAndDelete(req.params.id);
+
+	res.status(200).json({ success: true, data: {} });
+});
