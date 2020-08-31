3f15f5b7b8f04285d782b19b8f670e5126032bb5
JatinBumbra <56374584+JatinBumbra@users.noreply.github.com>
2020-08-31 13:00:43 +0530

    Update details and password

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,16 @@ const {
 	currentUser,
 	forgotPassword,
 	resetPassword,
+	updateDetails,
+	updatePassword,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', registerUser);
 router.post('/login', login);
 router.get('/me', protect, currentUser);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:token', resetPassword);
 
